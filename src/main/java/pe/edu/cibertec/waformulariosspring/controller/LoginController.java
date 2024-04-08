1a54219dc0c43e08ddf3fce34dadb99552bc53df
Luis <i202211476@cibertec.edu.pe>
2024-04-07 21:11:02 -0500

    Mensaje descriptivo de tus cambios

--- src/main/java/pe/edu/cibertec/waformulariosspring/controller/LoginController.java
+++ src/main/java/pe/edu/cibertec/waformulariosspring/controller/LoginController.java
@@ -2,11 +2,20 @@ package pe.edu.cibertec.waformulariosspring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import pe.edu.cibertec.waformulariosspring.models.LoginModel;
 
 @Controller
 public class LoginController {
     @GetMapping("/login")
-    public String login(){
+    public String login(Model model){
+        model.addAttribute("loginModel",new LoginModel());
         return "login";
+    }
+
+    public String validarUsuario(@ModelAttribute("loginModel") LoginModel loginModel , Model model){
+
+        return"login";
     }
 }
